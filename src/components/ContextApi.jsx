930b21d7428fd08497fa5f84706f57f49582d6bb
Github3121 <mdsani3121@gmail.com>
2026-08-11 14:32:32 +0600

    React Toastify Seted

--- src/components/ContextApi.jsx
+++ src/components/ContextApi.jsx
@@ -5,7 +5,7 @@ import { toast, ToastContainer } from 'react-toastify';
 export const ContextApi = createContext(null);
 
 export default function ContextProvider({ children }) {
-  const notify = () => toast('');
+  const notify = ( text ) => toast(text);
 
   return (
     <ContextApi.Provider value={notify}>
--- src/components/DetailActions.jsx
+++ src/components/DetailActions.jsx
@@ -4,22 +4,26 @@ import { ContextApi } from '@/components/ContextApi';
 import { MessageSquareMore, PhoneCall, Video } from 'lucide-react';
 
 export default function DetailActions() {
-  const notify = useContext(ContextApi);
+    const notify = useContext(ContextApi);
 
-  return (
-    <div className='flex justify-around items-center gap-4'>
-      <button
-        onClick={() => notify?.()}
-        className='btn w-55 h-23 grid p-4 text-[18px] rounded-[8px] text-[#1F2937]'
-      >
-        <PhoneCall size={32} />Call
-      </button>
-      <button className='btn w-55 h-23 grid p-4 text-[18px] rounded-[8px] text-[#1F2937]'>
-        <MessageSquareMore size={32} />Text
-      </button>
-      <button className='btn w-55 h-23 grid p-4 text-[18px] rounded-[8px] text-[#1F2937]'>
-        <Video size={32} />Video
-      </button>
-    </div>
-  );
+    return (
+        <div className='flex justify-around items-center gap-4'>
+            <button
+                onClick={() => notify?.('Tap to Call')}
+                className='btn w-55 h-23 grid p-4 text-[18px] rounded-[8px] text-[#1F2937]'
+            >
+                <PhoneCall size={32} />Call
+            </button>
+            <button
+                onClick={() => notify?.('Tap to Text')}
+                className='btn w-55 h-23 grid p-4 text-[18px] rounded-[8px] text-[#1F2937]'>
+                <MessageSquareMore size={32} />Text
+            </button>
+            <button
+                onClick={() => notify?.('Tap to Video')}
+                className='btn w-55 h-23 grid p-4 text-[18px] rounded-[8px] text-[#1F2937]'>
+                <Video size={32} />Video
+            </button>
+        </div>
+    );
 }
